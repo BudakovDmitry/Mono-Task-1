e6a89279db15c9e0150265f46077ba925aa50676
Budakov Dmitry <budakov.it@gmail.com>
2022-02-06 23:48:03 +0200

    update

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ function addThousandsSeparators(sum) {
     return sum.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
 }
 
-console.log(addThousandsSeparators(sum))
+addThousandsSeparators(sum);
 
 
